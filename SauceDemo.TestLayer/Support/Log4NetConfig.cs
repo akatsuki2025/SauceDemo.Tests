2260a961edc4169b9f4382afd5a7cdb168c4431a
Natalia Spyra-Stanuch <natspyra@gmail.com>
2025-11-17 22:58:52 +0100

    Logger refactoring

--- SauceDemo.TestLayer/Support/Log4NetConfig.cs
+++ SauceDemo.TestLayer/Support/Log4NetConfig.cs
@@ -1,4 +1,5 @@
 ﻿using log4net;
+using log4net.Appender;
 using log4net.Config;
 using System.Reflection;
 
@@ -10,6 +11,17 @@ namespace SauceDemo.TestLayer.Support
         {
             var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly());
             XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+
+            string logFileName = $"logs/test-log-{DateTime.Now:yyyyMMdd_HHmmss}.txt";
+
+            foreach (var appender in logRepository.GetAppenders())
+            {
+                if (appender is RollingFileAppender fileAppender)
+                {
+                    fileAppender.File = logFileName;
+                    fileAppender.ActivateOptions();
+                }
+            }
         }
     }
 }
--- SauceDemo.TestLayer/Support/WebDriverHooks.cs
+++ SauceDemo.TestLayer/Support/WebDriverHooks.cs
@@ -1,13 +1,22 @@
-﻿using SauceDemo.CoreLayer.Drivers.Enums;
+﻿using log4net;
+using SauceDemo.CoreLayer.Drivers.Enums;
 
 namespace SauceDemo.TestLayer.Support
 {
     [Binding]
     public class WebDriverHooks
     {
+        private readonly ScenarioContext _scenarioContext;
+
+        public WebDriverHooks(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
         [BeforeScenario]
         public void BeforeScenario()
         {
+            LogicalThreadContext.Properties["TestName"] = _scenarioContext.ScenarioInfo.Title;
             DriverManager.SetBrowser(BrowserType.Firefox);
             DriverManager.GetDriver();
         }
@@ -16,6 +25,7 @@ namespace SauceDemo.TestLayer.Support
         public void AfterScenario()
         {
             DriverManager.QuitDriver();
+            LogicalThreadContext.Properties.Remove("TestName");
         }
     }
 }
